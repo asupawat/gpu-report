98f787cbd4fbac7be662d0d50bf942b2fd953b10
Noom <asupawat@users.noreply.github.com>
2018-01-15 11:48:43 +0700

    Remove clocks.mem

--- gpu_report.sh
+++ gpu_report.sh
@@ -7,7 +7,7 @@ gpu_count=$((gpu_count-1))
 data="RIG2"$'\n'
 for((id=0;id<=$gpu_count;id++))
 do
-data+=$(nvidia-smi -i $id --query-gpu=index,name,temperature.gpu,fan.speed,clocks.gr,clocks.mem,power.draw --format=csv,noheader)$'\n\n'
+data+=$(nvidia-smi -i $id --query-gpu=index,name,temperature.gpu,fan.speed,clocks.gr,power.draw --format=csv,noheader)$'\n\n'
 done
 curl -X POST -d "$data" https://qwave.easyio.tech/gpu/gpu_report.php
 sleep 60
